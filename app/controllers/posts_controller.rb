e336fa9d31b219d2730c9c9c71b9c1df3b71c739
marcinrzepiel <marcinrzepiel@wp.pl>
2016-12-17 18:18:24 +0100

    Nothing to do here

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
+
   def create
     @post = Post.create(user: current_user, category: params[:category_id], description: params[:description])
     redirect_to category_path(params[:category_id])
-    end
   end
+  
 end
