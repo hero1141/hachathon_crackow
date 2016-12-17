1b74e0f88101dda064b5234add340f422e977182
marcinrzepiel <marcinrzepiel@wp.pl>
2016-12-17 14:42:52 +0100

    Script

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,16 @@ ActiveRecord::Schema.define(version: 20161217130741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "games", force: :cascade do |t|
+    t.integer  "time"
+    t.integer  "user_id"
+    t.integer  "distance"
+    t.integer  "points"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_games_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                             default: "", null: false
     t.string   "encrypted_password",                default: "", null: false
@@ -34,4 +44,5 @@ ActiveRecord::Schema.define(version: 20161217130741) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "games", "users"
 end
