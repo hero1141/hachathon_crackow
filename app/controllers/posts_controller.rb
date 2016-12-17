c663b79a857ce333d7a7afe75b902e5295f7619c
hero1141 <adzio96@gmail.com>
2016-12-17 18:17:20 +0100

    Categories

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,5 @@ class PostsController < ApplicationController
   def create
     @post = Post.create(user: current_user, category: params[:category_id], description: params[:description])
     redirect_to category_path(params[:category_id])
-    end
   end
 end
