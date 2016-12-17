dc41877693f2f400f79a144f59c00ac976929050
Glaadiss <bartekgladys@gmail.com>
2016-12-17 17:03:04 +0100

    add category and posts

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,26 @@
+class CategoriesController < ApplicationController
+  def index
+    @category = Category.new
+    @categories = Category.all
+  end
+
+  def create
+    @category = Category.new(name: params[:name])
+    if @category.save
+      redirect_to categories_path
+    else
+      render 'index'
+    end
+  end
+
+  def destroy
+    Category.find(params[:id]).destroy
+    redirect_to categories_path
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @posts = @category.posts
+    @post = Post.new
+  end
+end
