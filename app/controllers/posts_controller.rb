71d028c9b0c3df445d314d79e02930524d06cd43
marcinrzepiel <marcinrzepiel@wp.pl>
2016-12-17 19:11:54 +0100

    Some frontend

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,16 @@
 class PostsController < ApplicationController
+  def index 
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+
+  end
+
 
   def create
     @post = Post.create(user: current_user, category: params[:category_id], description: params[:description])
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,6 @@
 class StaticController < ApplicationController
   def index
     @posts = Post.all.limit(10).order('id desc')
+    @category = Category.all
   end
 end
