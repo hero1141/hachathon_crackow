550676d8fb72ec92b22b45f8bbe8358aaa05aa32
marcinrzepiel <marcinrzepiel@wp.pl>
2016-12-17 20:17:33 +0100

    Some forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,8 @@ class PostsController < ApplicationController
 
 
   def create
-    @post = Post.create(user: current_user, category: params[:category_id], description: params[:description])
-    redirect_to category_path(params[:category_id])
+    @post = Post.create(user: current_user, category_id: params[:post][:id], description: params[:post][:description])
+    redirect_to category_path(params[:post][:id])
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
+  
   resources :categories 
+  resources :posts 
   resources :games, only: [:index, :new, :create, :show]
   root 'static#index'
   devise_for :users
   get 'static/index'
+
 end
