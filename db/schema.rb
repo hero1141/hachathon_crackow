4f7923a0fd992d55d4af23548488322e7838db93
hero1141 <adzio96@gmail.com>
2016-12-18 00:06:53 +0100

    title post

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161217210031) do
-
+ActiveRecord::Schema.define(version: 20161217224502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,8 +86,9 @@ ActiveRecord::Schema.define(version: 20161217210031) do
     t.integer  "user_id"
     t.string   "description"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "title",       limit: 64
     t.index ["category_id"], name: "index_posts_on_category_id", using: :btree
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
@@ -107,7 +106,6 @@ ActiveRecord::Schema.define(version: 20161217210031) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                                     null: false
     t.datetime "updated_at",                                     null: false
-
     t.string   "first_name",             limit: 32,              null: false
     t.string   "last_name",              limit: 32,              null: false
     t.string   "avatar_file_name"
