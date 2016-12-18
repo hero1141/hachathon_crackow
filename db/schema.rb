0f131dc7474121b874a1eb4ef7d2c4a6d5d56492
marcinrzepiel <marcinrzepiel@wp.pl>
2016-12-18 01:11:23 +0100

    Siemka

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217185719) do
+ActiveRecord::Schema.define(version: 20161217224502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,14 +106,8 @@ ActiveRecord::Schema.define(version: 20161217185719) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                                     null: false
     t.datetime "updated_at",                                     null: false
-
-    t.string   "first_name",             limit: 32,              null: false
-    t.string   "last_name",              limit: 32,              null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
-
+    t.string   "first_name",             limit: 32
+    t.string   "last_name",              limit: 32
     t.string   "provider"
     t.string   "uid"
     t.string   "department"
